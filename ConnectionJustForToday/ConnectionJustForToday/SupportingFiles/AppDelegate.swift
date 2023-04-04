6380d5110b0ac302eff741576d5754d6c0b7c613
Matthew Hill <matthill265@gmail.com>
2023-04-04 14:25:58 -0600

    Removes mock data from the AppDelegate

--- ConnectionJustForToday/ConnectionJustForToday/SupportingFiles/AppDelegate.swift
+++ ConnectionJustForToday/ConnectionJustForToday/SupportingFiles/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        JFTMockModelController.sharedInstance.createJFT(title: "Vigilance", date: "January 1", quote: "We keep what we have only with vigilance... Basic Text, p. 57", body: "How do we remain vigilant about our recovery? First, by realizing that we have a disease we will always have. No matter how long we've been clean, no matter how much better our lives have become, no matter what the extent of our spiritual healing, we are still addicts. Our disease waits patiently, ready to spring the trap if we give it the opportunity.", aspiration: "Just for today: I will be vigilant, doing everything necessary to guard my recovery.")
         return true
     }
 
