26eb188eafb2cee1e013498aebee6a3cf1d06b60
Matthew Hill <matthill265@gmail.com>
2023-04-04 21:03:58 -0600

    Creates the JFTService

--- ConnectionJustForToday/ConnectionJustForToday/HelperFiles/JFTService.swift
+++ ConnectionJustForToday/ConnectionJustForToday/HelperFiles/JFTService.swift
@@ -11,4 +11,29 @@ import SwiftSoup
 
 struct JFTService {
     
+    func scrapeJFT(completion: @escaping(Result<JFTReading, NetworkError>) -> Void) {
+        guard let url = URL(string: "https://www.jftna.org/jft/") else { completion(.failure(.invalidURL)); return}
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {completion(.failure(.noData)); return}
+            do {
+                if let html = String(data: data, encoding: .utf8),
+                   let doc = try? SwiftSoup.parse(html) {
+                    let tdElements = try doc.select("body td")
+                    
+                    var values: [String] = []
+                    for el in tdElements {
+                        values.append(try el.text())
+                    }
+                    let keys = ["date", "title", "pageNumber", "quote", "reference", "body", "affirmation", "copyright"]
+                    let result = OrderedDictionary(uniqueKeysWithValues: zip(keys, values))
+                    let jftReading = JFTReading(date: result["date"] ?? "", title: result["title"] ?? "", pageNumber: result["pageNumber"] ?? "", quote: result["quote"] ?? "", reference: result["reference"] ?? "", body: result["body"] ?? "", afffirmation: result["affirmation"] ?? "", copyright: "copyright" )
+                    completion(.success(jftReading))
+                }
+            } catch {
+                print(error)
+                completion(.failure(.thrownError(error)))
+            }
+        }
+        task.resume()
+    }
 }
--- ConnectionJustForToday/ConnectionJustForToday/Models/JFTReading.swift
+++ ConnectionJustForToday/ConnectionJustForToday/Models/JFTReading.swift
@@ -15,18 +15,25 @@ class JFTReading {
     // MARK: - Properties
     var date: String
     var title: String
+    var pageNumber: String
     var quote: String
+    var reference: String
     var body: String
     var afffirmation: String
+    var copyright: String
     var postsJFT: [JFTPost]?
     var uuid: String
     
-    init(date: String, title: String, quote: String, body: String, afffirmation: String, posts: [JFTPost] = [], uuid: String = UUID().uuidString) {
+    init(date: String, title: String, pageNumber: String, quote: String, reference: String, body: String, afffirmation: String, copyright: String, posts: [JFTPost] = [], uuid: String = UUID().uuidString) {
+        
         self.date = date
         self.title = title
+        self.pageNumber = pageNumber
         self.quote = quote
+        self.reference = reference
         self.body = body
         self.afffirmation = afffirmation
+        self.copyright = copyright
         self.postsJFT = posts
         self.uuid = uuid
     }
