60454579a217e54f74645902e00eb2fcef0f24a4
Matthew Hill <matthill265@gmail.com>
2023-04-14 20:51:55 -0600

    Fixes the UI for the JFT scene and the segue to the post view

--- ConnectionJustForToday/ConnectionJustForToday/HelperFiles/JFTService.swift
+++ ConnectionJustForToday/ConnectionJustForToday/HelperFiles/JFTService.swift
@@ -50,9 +50,9 @@ struct JFTService {
                     break Decode
                 case .error:
                     print("Decoding error")
-                    let replacementCharacter = UnicodeScalar(0xFFFD)!
-                    sanitizedData.append(contentsOf: replacementCharacter.utf8)
-                    utf8Decoder = UTF8()
+//                    let replacementCharacter = UnicodeScalar(0xFFFD)!
+//                    sanitizedData.append(contentsOf: replacementCharacter.utf8)
+//                    utf8Decoder = UTF8()
                 }
             }
             return sanitizedData
--- ConnectionJustForToday/ConnectionJustForToday/Views/JFTReading/JFTReadingViewController.swift
+++ ConnectionJustForToday/ConnectionJustForToday/Views/JFTReading/JFTReadingViewController.swift
@@ -40,8 +40,7 @@ class JFTReadingViewController: UIViewController{
         guard let jft = viewModel.jftReading else { return }
         jftDateLabel.text = jft.date
         jftTitleLabel.text = jft.title
-        jftQuoteLabel.text = jft.reference
-        jftQuoteLabel.text = jft.quote + jft.pageNumber
+        jftQuoteLabel.text = "\(jft.quote) \(jft.reference)"
         jftBodyTextView.text = jft.body
         jftAffirmationLabel.text = jft.affirmation
         jftCopyrightLabel.text = jft.copyright
@@ -63,14 +62,14 @@ extension JFTReadingViewController: JFTReadingViewModelDelegate {
 
 extension JFTReadingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return viewModel.jftPosts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "jftPost", for: indexPath) as? JFTPostTableViewCell else {return UITableViewCell()}
         
-//        let post = viewModel.jftPosts[indexPath.row]
-//        cell.updateUI(with: post)
+        let post = viewModel.jftPosts[indexPath.row]
+        cell.updateUI(with: post)
         return cell
     }
 }
