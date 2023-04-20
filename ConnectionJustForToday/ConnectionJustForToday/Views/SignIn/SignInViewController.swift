6efe53b9d1b8c27287135905cf233af0089463e8
Matthew Hill <matthill265@gmail.com>
2023-04-20 16:31:46 -0600

    Functional apple sign in button

--- ConnectionJustForToday/ConnectionJustForToday/Views/SignIn/SignInViewController.swift
+++ ConnectionJustForToday/ConnectionJustForToday/Views/SignIn/SignInViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
     
@@ -16,6 +18,7 @@ class SignInViewController: UIViewController {
     
     // MARK: - Properties
     var viewModel: SignInUserViewModel!
+    var nonce: String?
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -36,12 +39,59 @@ class SignInViewController: UIViewController {
     
     // MARK: - Functions
     
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
     @objc func appleSignInButtonTapped() {
         let provider = ASAuthorizationAppleIDProvider()
         let request = provider.createRequest()
         request.requestedScopes = [
             .email
         ]
+        
+        self.nonce = randomNonceString()
+        request.nonce = sha256(nonce!)
+        
+        
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.presentationContextProvider = self
         authorizationController.delegate = self
@@ -67,10 +117,26 @@ extension SignInViewController: ASAuthorizationControllerPresentationContextProv
 extension SignInViewController: ASAuthorizationControllerDelegate {
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        <#code#>
+        
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        <#code#>
+        
+        if let appleIdCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            // Do something with this credential
+            guard let nonce = self.nonce else {
+                fatalError()
+            }
+            guard let appleIdToken = appleIdCredential.identityToken else {
+                print("failed to get token")
+                return
+            }
+            guard let idTokenString = String(data: appleIdToken, encoding: .utf8) else { print("No ID Token"); return}
+            let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            Auth.auth().signIn(with: firebaseCredential) { [weak self] authResult, error in
+                // Do something after firebase sign in has completed
+                
+            }
+        }
     }
 }
