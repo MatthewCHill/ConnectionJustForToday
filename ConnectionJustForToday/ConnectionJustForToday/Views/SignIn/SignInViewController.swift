135c84da0c3f700b315fbdfc53d5dd32ce35101d
Matthew Hill <matthill265@gmail.com>
2023-04-20 15:56:03 -0600

    Presents the apple sign in button

--- ConnectionJustForToday/ConnectionJustForToday/Views/SignIn/SignInViewController.swift
+++ ConnectionJustForToday/ConnectionJustForToday/Views/SignIn/SignInViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class SignInViewController: UIViewController {
     
@@ -21,6 +22,30 @@ class SignInViewController: UIViewController {
         super.viewDidLoad()
         viewModel = SignInUserViewModel()
         self.hideKeyboardWhenDone()
+        let signInWithApple = ASAuthorizationAppleIDButton()
+        signInWithApple.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(signInWithApple)
+        NSLayoutConstraint.activate([
+            signInWithApple.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            signInWithApple.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            signInWithApple.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -70),
+            signInWithApple.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        signInWithApple.addTarget(self, action: #selector(appleSignInButtonTapped), for: .touchUpInside)
+    }
+    
+    // MARK: - Functions
+    
+    @objc func appleSignInButtonTapped() {
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [
+            .email
+        ]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.presentationContextProvider = self
+        authorizationController.delegate = self
+        authorizationController.performRequests()
     }
     
     
@@ -29,5 +54,23 @@ class SignInViewController: UIViewController {
         guard let email = emailTextField.text,
               let password = passwordTextField.text else { return }
         viewModel.signIn(email: email, password: password)
+    }
+} // End of class
+
+// MARK: - Extensions
+
+extension SignInViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+extension SignInViewController: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        <#code#>
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        <#code#>
     }
 }
